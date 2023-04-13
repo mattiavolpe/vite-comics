b9039d2a0130dfe991a06ba9f5df4406904567f5
mattiavolpe <64392380+MatthewFox90@users.noreply.github.com>
2023-04-13 16:41:01 +0200

    ADD Header style

--- src/App.vue
+++ src/App.vue
@@ -51,18 +51,19 @@ export default {
 
 <template>
 
-  <header class="py-4">
-    <div class="container d-flex justify-content-between align-items-center">
+  <header class="d-flex align-items-center justify-content-center">
+    <div class="container d-flex justify-content-between align-items-center h-100">
       <div class="left">
         <img src="./assets/img/dc-logo.png" alt="DC Logo">
       </div>
-      <ul class="right d-flex m-0">
-        <li v-for="item in menuItems" class="mx-3">
+      <ul class="right d-flex m-0 p-0 h-100">
+        <li v-for="(item, index) in menuItems" :class="index === 0 ? 'active' : ''" class="d-flex align-items-center mx-3 h-100">
           <a :href="item.url">{{ item.title.toUpperCase() }}</a>
         </li>
       </ul>
     </div>
   </header>
+
 </template>
 
 <style>
