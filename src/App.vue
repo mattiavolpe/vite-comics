1ad3e8012defded962d1c72d9136b5a0366222a1
mattiavolpe <64392380+MatthewFox90@users.noreply.github.com>
2023-04-13 15:43:20 +0200

    ADD Bootstrap, SCSS and header content

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,68 @@
 <script>
+export default {
+  data() {
+    return {
+      menuItems: [
+        {
+          title: "Characters",
+          url: "#",
+        },
+        {
+          title: "Comics",
+          url: "#",
+        },
+        {
+          title: "Movies",
+          url: "#",
+        },
+        {
+          title: "TV",
+          url: "#",
+        },
+        {
+          title: "Games",
+          url: "#",
+        },
+        {
+          title: "Collectibles",
+          url: "#",
+        },
+        {
+          title: "Videos",
+          url: "#",
+        },
+        {
+          title: "Fans",
+          url: "#",
+        },
+        {
+          title: "News",
+          url: "#",
+        },
+        {
+          title: "Shop",
+          url: "#",
+        },
+      ]
+    }
+  }
+}
 </script>
 
 <template>
-  
+
+  <header class="py-4">
+    <div class="container d-flex justify-content-between align-items-center">
+      <div class="left">
+        <img src="./assets/img/dc-logo.png" alt="DC Logo">
+      </div>
+      <ul class="right d-flex m-0">
+        <li v-for="item in menuItems" class="mx-3">
+          <a :href="item.url">{{ item.title.toUpperCase() }}</a>
+        </li>
+      </ul>
+    </div>
+  </header>
 </template>
 
 <style>
