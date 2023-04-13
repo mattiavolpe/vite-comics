9184f3fd9e7032e9051aa740e553a39ce869c051
mattiavolpe <64392380+MatthewFox90@users.noreply.github.com>
2023-04-13 17:18:29 +0200

    ADD External sites content

--- src/App.vue
+++ src/App.vue
@@ -43,6 +43,33 @@ export default {
           title: "Shop",
           url: "#",
         },
+      ],
+      externalSites: [
+        {
+          img: "./src/assets/img/buy-comics-digital-comics.png",
+          title: "Digital comics",
+          link: "#",
+        },
+        {
+          img: "./src/assets/img/buy-comics-merchandise.png",
+          title: "DC merchandise",
+          link: "#",
+        },
+        {
+          img: "./src/assets/img/buy-comics-subscriptions.png",
+          title: "Subscription",
+          link: "#",
+        },
+        {
+          img: "./src/assets/img/buy-comics-shop-locator.png",
+          title: "Comic shop locator",
+          link: "#",
+        },
+        {
+          img: "./src/assets/img/buy-dc-power-visa.png",
+          title: "DC power visa",
+          link: "#",
+        },
       ]
     }
   }
@@ -56,7 +83,7 @@ export default {
       <div class="left">
         <img src="./assets/img/dc-logo.png" alt="DC Logo">
       </div>
-      <ul class="right d-flex m-0 p-0 h-100">
+      <ul class="right d-flex h-100">
         <li v-for="(item, index) in menuItems" :class="index === 0 ? 'active' : ''" class="d-flex align-items-center mx-3 h-100">
           <a :href="item.url">{{ item.title.toUpperCase() }}</a>
         </li>
@@ -66,10 +93,18 @@ export default {
 
   <main>
     <section id="main_section" class="bg-dark">
-        <div class="container">
-          <h1 class="m-0 py-5 text-light">--&gt; Content goes here &lt;--</h1>
-        </div>
-      </section>
+      <div class="container">
+        <h1 class="m-0 py-5 text-light">--&gt; Content goes here &lt;--</h1>
+      </div>
+    </section>
+    <section id="external_sites">
+      <ul class="container d-flex justify-content-between align-items-center py-5 text-light">
+        <li v-for="site in externalSites" class="mx-4">
+          <img height="70" :src="site.img" :alt="site.title.toUpperCase() + ' Image'" class="me-2">
+          <a :href="site.link" class="text-light">{{ site.title.toUpperCase() }}</a>
+        </li>
+      </ul>
+    </section>
   </main>
 
 </template>
