d9c5d9ef211f2b5adcf2967b8f2cf5c44e6233d3
mattiavolpe <64392380+MatthewFox90@users.noreply.github.com>
2023-04-13 17:55:31 +0200

    ADD footer banner

--- src/App.vue
+++ src/App.vue
@@ -160,6 +160,41 @@ export default {
         </div>
       </div>
     </section>
+    <div id="bottom_banner">
+      <div class="container d-flex justify-content-between align-items-center">
+        <button>SIGN-UP NOW!</button>
+        <div class="d-flex align-items-center">
+          <h4 class="m-0 me-5">FOLLOW US</h4>
+          <ul class="d-flex align-items-center">
+            <li>
+              <a href="#">
+                <img src="./assets/img/footer-facebook.png" alt="">
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <img src="./assets/img/footer-twitter.png" alt="">
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <img src="./assets/img/footer-youtube.png" alt="">
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <img src="./assets/img/footer-pinterest.png" alt="">
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <img src="./assets/img/footer-periscope.png" alt="">
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </footer>
 
 </template>
