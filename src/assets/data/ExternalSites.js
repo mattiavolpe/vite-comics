b004ef57e7aac1d1fa50927bf6b22ff5526bf06d
mattiavolpe <64392380+MatthewFox90@users.noreply.github.com>
2023-04-14 11:21:49 +0200

    FIX dynamic images handling

--- src/assets/data/ExternalSites.js
+++ src/assets/data/ExternalSites.js
@@ -1,26 +1,26 @@
 export default [
   {
-    img: "./src/assets/img/buy-comics-digital-comics.png",
+    img: "buy-comics-digital-comics.png",
     title: "Digital comics",
     link: "#",
   },
   {
-    img: "./src/assets/img/buy-comics-merchandise.png",
+    img: "buy-comics-merchandise.png",
     title: "DC merchandise",
     link: "#",
   },
   {
-    img: "./src/assets/img/buy-comics-subscriptions.png",
+    img: "buy-comics-subscriptions.png",
     title: "Subscription",
     link: "#",
   },
   {
-    img: "./src/assets/img/buy-comics-shop-locator.png",
+    img: "buy-comics-shop-locator.png",
     title: "Comic shop locator",
     link: "#",
   },
   {
-    img: "./src/assets/img/buy-dc-power-visa.png",
+    img: "buy-dc-power-visa.png",
     title: "DC power visa",
     link: "#",
   },
--- src/components/ExternalSites.vue
+++ src/components/ExternalSites.vue
@@ -6,6 +6,11 @@
       return {
         ExternalSites,
       }
+    },
+    methods: {
+      getImageUrl(img) {
+        return new URL(`../assets/img/${img}`, import.meta.url).href;
+      }
     }
   }
 </script>
@@ -14,7 +19,7 @@
   <section id="external_sites">
     <ul class="container d-flex justify-content-between align-items-center py-5 text-light">
       <li v-for="site in ExternalSites" class="mx-4">
-        <img height="70" :src="site.img" :alt="site.title.toUpperCase() + ' Image'" class="me-2">
+        <img height="70" :src="getImageUrl(site.img)" :alt="site.title.toUpperCase() + ' Image'" class="me-2">
         <a :href="site.link" class="text-light">{{ site.title.toUpperCase() }}</a>
       </li>
     </ul>
